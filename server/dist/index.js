1a0ef0dff8ed001cd89336a47f7c2a64a771a109
jatinag17 <jatinagrawal128@gmail.com>
2025-02-06 00:04:57 +0530

    Fix log message to use consistent casing for PORT in server startup

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,4 +5,4 @@ const PORT = process.env.PORT || 7000;
 app.get('/', (req, res) => {
     res.send("Hey It's working...");
 });
-app.listen(PORT, () => console.log('Server is running on port ${PORT}'));
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`));
--- server/src/index.ts
+++ server/src/index.ts
@@ -7,4 +7,4 @@ const PORT = process.env.PORT || 7000;
 app.get('/',(req: Request,res: Response) => {
     res.send("Hey It's working...");
 });
- app.listen(PORT,() =>console.log('Server is running on port ${PORT}'));
+ app.listen(PORT,() =>console.log(`Server is running on PORT ${PORT}`));
