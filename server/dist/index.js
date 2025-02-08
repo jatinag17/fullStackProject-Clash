e331243d58870c78cf06c74abf978436547af208
jatinag17 <jatinagrawal128@gmail.com>
2025-02-08 16:59:25 +0530

    Add middleware for JSON and URL-encoded request parsing

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 7000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
     res.send("Hey It's working...");
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,10 @@ import "dotenv/config"
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
 
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
 app.get('/',(req: Request,res: Response) => {
     res.send("Hey It's working...");
 });
