f52f2bdf4438b3d1a8b74c399165e36e81296b17
jatinag17 <jatinagrawal128@gmail.com>
2025-02-08 17:37:31 +0530

    Set EJS as the view engine and configure views directory

--- server/dist/index.js
+++ server/dist/index.js
@@ -2,8 +2,14 @@ import express from 'express';
 import "dotenv/config";
 const app = express();
 const PORT = process.env.PORT || 7000;
+import path from 'path';
+import { fileURLToPath } from 'url';
+const _dirname = path.dirname(fileURLToPath(import.meta.url));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+//* set View engine
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, './views'));
 app.get('/', (req, res) => {
     res.send("Hey It's working...");
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,20 @@ import "dotenv/config"
 
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const _dirname=path.dirname(fileURLToPath(import.meta.url));
+
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
+
+//* set View engine
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, './views'));
 
 
 app.get('/',(req: Request,res: Response) => {
