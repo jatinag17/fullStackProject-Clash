68f3ab4531b9c6f6c7a2b1c966e7471320685f73
jatinag17 <jatinagrawal128@gmail.com>
2025-02-15 00:08:32 +0530

    Add Redis queue configuration and integrate email job processing

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,8 +17,14 @@ app.set('view engine', 'ejs');
 app.set('views', path.resolve(__dirname, './views'));
 app.get('/',async (req: Request,res: Response) => {
     const html = await ejs.renderFile(__dirname + `/views/emails/welcome.ejs`, {name: "Jatin Agrawal"})
-    await sendEmail("sedod75013@sectorid.com", "Testing SMTP", html);
-    res.json({msg:"Email send successfully"});
+    // await sendEmail("sedod75013@sectorid.com", "Testing SMTP", html);
+    await emailQueue.add(emailQueueName, {to:"sedod75013@sectorid.com", subject:"Testing queue email",body:"html"});
+     res.json({msg:"Email send successfully"});
 });
+
+//* Queues
+
+import "./jobs/EmailJob.js";
+import { emailQueue, emailQueueName } from './jobs/EmailJob.js';
 
  app.listen(PORT,() =>console.log(`Server is running on PORT ${PORT}`));
