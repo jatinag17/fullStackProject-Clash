326125989e89575ce5aed1404af1ea0105f64e65
jatinag17 <jatinagrawal128@gmail.com>
2025-02-20 18:22:01 +0530

    Update confirm password field casing in validation and add Toaster component to layout

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -2,11 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 import {Inter as FontSans} from "next/font/google"
 import {cn} from "@/lib/utils"
-
+import { Toaster } from "@/components/ui/sonner";
 const fontSans=FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 export const metadata: Metadata = {
   title: "Clashing App",
@@ -20,7 +20,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-     <body className={cn("min-h-screen bg-slate-100 font-sans antialiased",fontSans.variable)}>{children}</body>
+     <body 
+     className={cn(
+      "min-h-screen bg-slate-100 font-sans antialiased",
+      fontSans.variable
+      )}
+      >
+        <Toaster richColors position="top-right" />
+        {children}
+        </body>
     </html>
   );
 }
--- server/dist/validation/authValidation.js
+++ server/dist/validation/authValidation.js
@@ -3,5 +3,5 @@ export const registerSchema = z.object({
     name: z.string({ message: "Name is required." }).min(3, { message: "Name must be 3 characters long" }),
     email: z.string({ message: "Email is required." }).email({ message: "Please type correct email." }),
     password: z.string({ message: "Password is required." }).min(6, { message: "Password must be 6 characters long" }),
-    confirm_Password: z.string({ message: "Confirm Password is required." }).min(6, { message: "Confirm Password must be 6 characters long" }),
-}).refine((data) => data.password === data.confirm_Password, { message: "Password and Confirm Password must be same.", path: ["confirm_Password"] });
+    confirm_password: z.string({ message: "Confirm Password is required." }).min(6, { message: "Confirm Password must be 6 characters long" }),
+}).refine((data) => data.password === data.confirm_password, { message: "Password and Confirm Password must be same.", path: ["confirm_password"] });
