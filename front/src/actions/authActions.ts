0e2bfd625f28bf9fe32e58e15961d6cdb298f91f
jatinag17 <jatinagrawal128@gmail.com>
2025-02-20 17:14:39 +0530

    Add registerAction function for user registration handling

---
+++ front/src/actions/authActions.ts
@@ -0,0 +1,37 @@
+"use server"
+
+import { REGISTER_URL } from "@/lib/apiEndPoints"
+import axios, { AxiosError } from "axios"
+
+export async function registerAction(prevState:any, formData:FormData){
+     
+    try{
+      const {data}= await axios.post(REGISTER_URL,formData)
+        return {
+            status: 200,
+            message:data?.message?? "Account created successfully! Please check your email and verify your email",
+            errors: {},
+        }
+        }catch(error){
+            if(error instanceof AxiosError){
+                if(error.response?.status === 422){
+                return {
+                    status: 422,
+                    message: error.response?.data?.message,
+                    errors:error.response?.data.errors,
+                }
+            }
+        }
+        return {
+          status: 500,
+          message: "Something went wrong.please try again!",
+          errors:{},
+        };
+
+          return {
+            status: 200,
+            message: "Submitted",
+            errors: {},
+          };
+    }
+}
