f743ea9f18a155f65ae61a326d499379b75868e6
jatinag17 <jatinagrawal128@gmail.com>
2025-02-22 23:53:03 +0530

    Add authentication middleware for JWT token verification

---
+++ server/dist/middleware/AuthMiddleware.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (authHeader === null || authHeader === undefined) {
+        res.status(401).json({ status: 401, message: "Unauthorized" });
+    }
+    const token = authHeader.split(" ")[1];
+    //* Verify token
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+        if (err)
+            res.status(401).json({ status: 401, message: "Unauthorized" });
+        req.user = user;
+        next();
+    });
+};
+export default authMiddleware;
--- server/src/middleware/AuthMiddleware.ts
+++ server/src/middleware/AuthMiddleware.ts
@@ -4,16 +4,16 @@ import { Request, Response, NextFunction } from "express";
 const authMiddleware =(req:Request,res:Response,next:NextFunction)=>{
     const authHeader = req.headers.authorization
     if(authHeader ===null || authHeader === undefined){
-        return res.status(401).json({status:401,message:"Unauthorized"})
+         res.status(401).json({status:401,message:"Unauthorized"})
     }
     const token = authHeader.split(" ")[1]
 
     //* Verify token
     jwt.verify(token,process.env.SECRET_KEY!,(err,user)=>{
         if(err)
-            return res.status(401).json({status:401,message:"Unauthorized"})
+             res.status(401).json({status:401,message:"Unauthorized"})
         req.user = user as AuthUser
-        next();  
+        next(); 
     });
 }
 
