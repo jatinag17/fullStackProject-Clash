0037da8bd9e7a6f04ca10102a1433f5189d0e5a2
jatinag17 <jatinagrawal128@gmail.com>
2025-02-22 14:14:21 +0530

    Add login schema validation for email and password

--- server/dist/validation/authValidation.js
+++ server/dist/validation/authValidation.js
@@ -5,3 +5,8 @@ export const registerSchema = z.object({
     password: z.string({ message: "Password is required." }).min(6, { message: "Password must be 6 characters long" }),
     confirm_password: z.string({ message: "Confirm Password is required." }).min(6, { message: "Confirm Password must be 6 characters long" }),
 }).refine((data) => data.password === data.confirm_password, { message: "Password and Confirm Password must be same.", path: ["confirm_password"] });
+//* Login Schema
+export const loginSchema = z.object({
+    email: z.string({ message: "Email is required." }).email({ message: "Please type correct email." }),
+    password: z.string({ message: "Password is required." }).min(6, { message: "Password must be 6 characters long" }),
+});
