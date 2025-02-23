276e994c1aa5c3c0527c8f8a3349a4436fb64f7a
jatinag17 <jatinagrawal128@gmail.com>
2025-02-23 15:00:47 +0530

    Refactor login page to use Login component for improved structure and maintainability

--- front/src/app/(auth)/login/page.tsx
+++ front/src/app/(auth)/login/page.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Label } from '@/components/ui/label';
-import { Input } from '@/components/ui/input';
 import Link from "next/link";
+import Login from '@/components/auth/Login';
 export default function login() {
   return (
     <div className="flex justify-center items-center h-screen">
@@ -11,35 +10,8 @@ export default function login() {
         </h1>
         <h1 className="text-2xl font-bold">Login</h1>
         <p>Welcome Back</p>
-        <form>
-          <div className="mt-4">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Enter your email..."
-            />
-          </div>
-          <div className="mt-4">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Enter your password..."
-            />
-            <div className='text-right'>
-              <Link href="forgot-password"className="text-blue-600 hover:text-blue-800">Forgot Password?</Link>
-            </div>
-          </div>
 
-          <div className="mt-4">
-            <button className="w-full bg-gradient-to-r from-pink-500 to-purple-800 text-white py-2 rounded-lg">
-              Submit
-            </button>
-          </div>
-        </form>
+       <Login />
         <p className='text-center mt-2'>Don't have an account?{" "}<strong><Link href="/register">Register</Link></strong></p>
       </div>
     </div>
