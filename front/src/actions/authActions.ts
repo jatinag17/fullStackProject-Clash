b323dbf1d1441f4781d41ad690d272817ecfca8d
jatinag17 <jatinagrawal128@gmail.com>
2025-02-23 12:14:00 +0530

    Add loginAction function for user authentication

--- front/src/actions/authActions.ts
+++ front/src/actions/authActions.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { REGISTER_URL } from "@/lib/apiEndPoints"
+import { REGISTER_URL,LOGIN_URL } from "@/lib/apiEndPoints"
 import axios, { AxiosError } from "axios"
 
 export async function registerAction(prevState:any, formData:FormData){
@@ -35,4 +35,35 @@ export async function registerAction(prevState:any, formData:FormData){
 
          
     }
+}
+
+export async function loginAction(prevState: any, formData: FormData) {
+  try {
+    const { data } = await axios.post(LOGIN_URL, {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      });
+    return {
+      status: 200,
+      message:
+        data?.message ??
+        "Login you.",
+      errors: {},
+    };
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      if (error.response?.status === 422) {
+        return {
+          status: 422,
+          message: error.response?.data?.message,
+          errors: error.response?.data.errors,
+        };
+      }
+    }
+    return {
+      status: 500,
+      message: "Something went wrong.please try again!",
+      errors: {},
+    };
+  }
 }
