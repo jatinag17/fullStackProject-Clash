e23d41310549da13c3f4d95f3ebdc43e498515b6
jatinag17 <jatinagrawal128@gmail.com>
2025-02-24 23:51:02 +0530

    Add reset password validation schema with email, token, and password requirements

--- server/src/validation/passwordValidation.ts
+++ server/src/validation/passwordValidation.ts
@@ -5,3 +5,22 @@ export const forgetPasswordSchema=z.object({
     .string({message: "Email is required."})
     .email({message: "Please type correct email."}),
    });
+
+   export const resetPasswordSchema = z
+     .object({
+        email: z
+         .string({ message: "Email is required." })
+         .email({ message: "Please type correct email." }),
+        token: z
+         .string({ message: "Token is required." }),
+       password: z
+         .string({ message: "Password is required." })
+         .min(6, { message: "Password must be 6 characters long" }),
+       confirm_password: z
+         .string({ message: "Confirm Password is required." })
+         .min(6, { message: "Confirm Password must be 6 characters long" }),
+     })
+     .refine((data) => data.password === data.confirm_password, {
+       message: "Password and Confirm Password must be same.",
+       path: ["confirm_password"],
+     });
