d35f3180105b3c191e6355a82546d0328548627c
jatinag17 <jatinagrawal128@gmail.com>
2025-02-25 00:10:56 +0530

    Refactor formatting in App component for consistency

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import HeroSection from '@/components/ui/base/HeroSection';
-import { getServerSession } from 'next-auth';
-import { authOptions } from './api/auth/[...nextauth]/options';
+import React from "react";
+import HeroSection from "@/components/ui/base/HeroSection";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/options";
 export default async function App() {
   const session = await getServerSession(authOptions);
   return (
     <div>
       <p>{JSON.stringify(session)}</p>
-      <HeroSection/>
+      <HeroSection />
     </div>
-  )
+  );
 }
