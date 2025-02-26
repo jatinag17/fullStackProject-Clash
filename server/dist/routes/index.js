e8da040ddfce6c7d668789a0dcf2f714391cf87d
jatinag17 <jatinagrawal128@gmail.com>
2025-02-26 16:56:33 +0530

    Add password routes to the authentication API

--- server/dist/routes/index.js
+++ server/dist/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import AuthRoutes from './authRoutes.js';
+import PasswordRoutes from './passwordRoutes.js';
 import VerifyRoutes from './verifyRoutes.js';
 const router = Router();
 router.use("/api/auth", AuthRoutes);
+router.use("/api/auth", PasswordRoutes);
 router.use("/", VerifyRoutes);
 export default router;
