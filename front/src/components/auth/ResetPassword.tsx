3cdfb79544959a29c8928f60ef6b23391ed07194
jatinag17 <jatinagrawal128@gmail.com>
2025-02-27 01:01:24 +0530

    Add Reset Password component for user password reset functionality

---
+++ front/src/components/auth/ResetPassword.tsx
@@ -0,0 +1,58 @@
+"use client";
+import React, { useEffect } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { SubmitButton } from "@/components/common/SubmitButton";
+import { useFormState } from "react-dom";
+import { registerAction } from "@/actions/authActions";
+import { toast } from "sonner";
+import { useSearchParams } from "next/navigation";
+export default function ResetPassword() {
+  const initState = {
+    message: "",
+    status: 0,
+    errors: {},
+  };
+  const [state, formAction] = useFormState(registerAction, initState);
+  const sParasm=useSearchParams();
+
+  useEffect(() => {
+    if (state.status === 404) {
+      toast.error(state.message);
+    } else if (state.status === 200) {
+      toast.success(state.message);
+    }
+  }, [state]);
+  return (
+    <form action={formAction}>
+     <input type="hidden"  name="token" value={sParasm.get("token") ?? ""}/>
+      <div className="mt-4">
+        <Label htmlFor="email">Email</Label>
+        <Input placeholder="Type your email" name="email" readOnly
+        value={sParasm.get("email") ?? ""}/>
+        <span className="text-red-400">{state.errors?.email}</span>
+      </div>
+      <div className="mt-4">
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          placeholder="Type your password"
+          name="password"
+        />
+        <span className="text-red-400">{state.errors?.password}</span>
+      </div>
+      <div className="mt-4">
+        <Label htmlFor="cpassword">Confirm Password</Label>
+        <Input
+          type="password"
+          placeholder="Type your password"
+          name="confirm_password"
+        />
+        <span className="text-red-400">{state.errors?.confirm_password}</span>
+      </div>
+      <div className="mt-4">
+        <SubmitButton />
+      </div>
+    </form>
+  );
+}
